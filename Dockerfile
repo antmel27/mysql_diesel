9667b5e15abcef53d5197adb64a379816f9e999d
Sebastian <sebastian@akerhielm.nu>
2023-04-23 20:16:33 +0200

    skelett för säkerhet och user specific saker

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN apt-get -y update && apt-get install -y libmariadb-dev && apt-get install -y
 COPY --from=builder /usr/local/cargo/bin/mysql_diesel /usr/local/bin/mysql_diesel
 ENV ROCKET_ADDRESS=0.0.0.0
 ENV DATABASE_URL=mysql://admin:KGvm8i3he28Zv7X@database-test.cco6ewvqipkm.eu-north-1.rds.amazonaws.com:3306/courselend-db
-
+ENV TOTP_SECRET=1234
 EXPOSE 8000
 CMD ["mysql_diesel"]
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use tables::Book;
 use diesel::{prelude::*, update};
 use dotenv::dotenv;
 use rocket::*;
-use std::env;
+use std::{env, time::SystemTime};
+use sha256::*;
 
 pub fn create_connection() -> MysqlConnection {
     dotenv().ok();
@@ -53,8 +54,12 @@ fn get_book_bid(bid_input: i32) -> String {
         Err(_) => serde_json::to_string("Error retrieving book").unwrap(),
     }
 }
-#[get("/increase/<isbn>/<quantity>")]
-fn increase_stock(isbn: String, quantity: i32) {
+#[get("/increase/<isbn>/<quantity>/<totp>")]
+fn increase_stock(isbn: String, quantity: i32, totp: String) {
+    if !totp_gen().contains(&totp) {
+        return
+    }
+
     use schema::books::dsl::*; //Get the books table.
     let isbn_clone = isbn.clone();
     let connection = &mut create_connection(); //Establish connection
@@ -69,8 +74,12 @@ fn increase_stock(isbn: String, quantity: i32) {
         .execute(connection);
 }
 
-#[get("/decrease/<isbn>")] // Decreases stock by 1.
-fn decrease_stock(isbn: String) {
+#[get("/decrease/<isbn>/<totp>")] // Decreases stock by 1.
+fn decrease_stock(isbn: String, totp: String) {
+    if !totp_gen().contains(&totp) {
+        return
+    }
+
     use schema::books::dsl::*; //Get the books table.
     let isbn_clone = isbn.clone();
     let connection = &mut create_connection(); //Establish connection
@@ -84,9 +93,38 @@ fn decrease_stock(isbn: String) {
         .set(stock.eq(new_stock)) //Set new stock value.
         .execute(connection);
 }
+
+#[get("/user/<uid>")]
+fn user_books(uid: String) -> String {
+    todo!()
+}
+
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/books", routes![get_books, get_book_isbn, get_book_bid, increase_stock, decrease_stock])
+
+}
+
+fn totp_gen() -> Vec<String> {
+    let current_tm = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        Err(_) => panic!("fuck")
+    };
+    let tm_0 = ((current_tm-30)/30).to_string();
+    let tm_1 = (current_tm/30).to_string();
+    let tm_2 = ((current_tm+30)/30).to_string();
+
+    let key = env::var("TOTP_SECRET").expect("uhhhhhhhhh");
+    let totp_0 = digest(format!("{}{}", key, tm_0));
+    let totp_1 = digest(format!("{}{}", key, tm_1));
+    let totp_2 = digest(format!("{}{}", key, tm_2));
+
+    vec![
+        totp_0[totp_0.len()-6..totp_0.len()].to_string(), 
+        totp_1[totp_1.len()-6..totp_1.len()].to_string(), 
+        totp_2[totp_2.len()-6..totp_2.len()].to_string()
+        ]
 
 }
