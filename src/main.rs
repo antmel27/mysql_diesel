304486eb71052665bac8d70ac1447b015a97f5d9
antmel27 <antmasta27@gmail.com>
2023-05-11 13:54:00 +0200

    push

--- src/main.rs
+++ src/main.rs
@@ -94,9 +94,6 @@ fn get_related_course_by_id(bid: i32) -> String
 }
 #[get("/increase/<isbn>/<quantity>")]
 fn increase_stock(isbn: String, quantity: i32) {
-/*     if !totp_gen().contains(&totp) {
-        return
-    } */
 
     use schema::books::dsl::*; //Get the books table.
     let isbn_clone = isbn.clone();
@@ -133,7 +130,7 @@ fn decrease_stock(isbn: String) {
 }
 
 
-#[get("/borrow/<address>/<husnummer>/<postkod>/<stad>/<isbn>/<token>")]
+#[get("/borrow/<name_of_reciever>/<address>/<husnummer>/<postkod>/<stad>/<isbn>/<token>")]
 fn borrow_book(address: String, husnummer: String, postkod: i32, stad: String, isbn: String, token: String) -> String
 { //Function for a user to borrow book.
     use schema::books::dsl::*;
@@ -181,13 +178,111 @@ fn borrow_book(address: String, husnummer: String, postkod: i32, stad: String, i
     }
     //Check what period course is in.
 } 
-/* #[get("/userbooks/<token>")] //Function to get all books
-fn get_userbooks(token: String) -> String
+
+#[get("/userbooks/<token>")] //Function to get all books
+fn get_userbooks(token: String) 
 {
+    use schema::userbooks::dsl::*;
+
     //Get the book id's from all the userbook tables, use them to search through the 
 
-} */
+} 
 
+#[get("/return/<isbn>/<token>")]
+fn return_book(isbn: String, token: String) -> String { 
+    use schema::books::dsl::*;
+    use schema::userbooks::dsl::*;
+    use schema::userbooks::columns::user_id;
+    use schema::users::dsl::*;
+    //Return book
+
+    //Authenticate
+    let isbn_clone = isbn.clone(); //Clone because isbn is moved later.
+    let connection = &mut create_connection(); //Establish connection
+    
+    let associated_user_option: Result<User, diesel::result::Error> = users.filter(uid.eq(token)).first::<User>(connection);
+    let associated_user = match associated_user_option {
+        Ok(associated_user_result) => associated_user_result,
+        Err(err) => return format!("{}", err),
+    }; //Find associated user
+    
+    let associated_book_option: Result<Book, diesel::result::Error> = books.filter(isbn13.eq(isbn)).first::<Book>(connection);
+    let associated_book = match associated_book_option {
+        Ok(associated_book_result) => associated_book_result,
+        Err(err) => return format!("{}", err),
+    }; //Get the associated book.
+
+    //Get the terms to search userbooks table with.
+    let search_book_id = associated_book.bid;
+    let search_user_id_int = associated_user.user_id;
+
+    let associated_userbook_option = diesel::delete(userbooks.filter(book_id.eq(search_book_id)).filter(user_id.eq(search_user_id_int))).execute(connection);
+    match associated_userbook_option {
+        Ok(result) => {
+            increase_stock(isbn_clone, 1);
+            return format!("{}", result)
+    },
+    Err(err) => return format!("{}", err),
+    };
+    //Filter via book and user id
+    //Remove entry from usertable, 
+    //increase stock of the book by 1.
+
+
+}
+
+#[get("/buy/<isbn>/<token>")]
+fn sell_book(isbn: String, token: String) {
+
+    //User authentication
+    increase_stock(isbn, 1); //Increase the stock by one.
+}
+
+#[get("/books/next-period/<token>")] //Function that returns the books the user will need in the next period.
+fn get_next_period_books(token: String) -> String
+{
+    use schema::users::dsl::*;
+    use schema::books::dsl::*;
+    use schema::coursebooks::dsl::*;
+    use schema::courses::dsl::*;
+
+    let connection = &mut create_connection(); //Establish connection
+    let related_user: User = users.filter(uid.eq(token)).first::<User>(connection).unwrap(); //Get the related user.
+    let today = Local::now().date_naive();
+    let user_period = match today {
+        d if today >= NaiveDate::from_ymd_opt(2023, 3, 27).unwrap() && today <= NaiveDate::from_ymd_opt(2023, 6, 4).unwrap() => 4,
+        d if today >= NaiveDate::from_ymd_opt(2023, 8, 28).unwrap() && today <= NaiveDate::from_ymd_opt(2023, 11, 5).unwrap() => 1,
+        d if today >= NaiveDate::from_ymd_opt(2023, 11, 6).unwrap() && today <= NaiveDate::from_ymd_opt(2024, 1, 14).unwrap() => 2,
+        d if today >= NaiveDate::from_ymd_opt(2024, 1, 15).unwrap() && today <= NaiveDate::from_ymd_opt(2024, 3, 24).unwrap() => 3,
+        _ => return format!("Date outside of known periods")
+    }; //Get the user period.
+
+    let user_current_term = related_user.term;
+    let next_period_courses_result = match user_period {
+        2 => courses.filter(period_start.eq(3)).filter(term_start.eq(user_current_term + 1)).load::<Course>(connection), //If user is currently in period 2, show next term courses and period 3.
+        4 => courses.filter(period_start.eq(1)).filter(term_start.eq(user_current_term + 1)).load::<Course>(connection), //If user is currently in period 4, show courses from beginning of next year.
+        _ => courses.filter(period_start.eq(user_period + 1)).filter(term_start.eq(user_current_term)).load::<Course>(connection) //If user is period 1 or 3, show next period courses but same term.
+    }; //Get all the courses for the user in the next period (vector).
+    let next_period_courses = match next_period_courses_result {
+        Ok(courses_result) => courses_result,
+        Err(err) => return format!("{}", err),
+    };
+    let mut next_period_books = String::new(); //String with next period books.
+
+    for course in next_period_courses
+    {
+        //ERROR -- PROBLEM IS WHAT TO DO IF NO RESULT
+        let coursebook_result = coursebooks.filter(coursebook_course_id.eq(course.cid)).first::<Coursebook>(connection);         //Find the related coursebook entry and get book id.
+        let coursebook: Coursebook = match coursebook_result {
+            Ok(coursebook_return) => coursebook_return, //If find
+            Err(_) => continue, //Need to skip this somehow.
+        };
+        let next_period_book: Book = books.filter(bid.eq(coursebook.coursebook_book_id)).first::<Book>(connection).unwrap(); //Find the related book.
+        next_period_books.push_str(&serde_json::to_string(&next_period_book).unwrap()); //Convert to string and push. 
+
+    }; 
+    next_period_books
+} 
 
 
 #[launch]
@@ -200,6 +295,10 @@ fn rocket() -> _ {
         .mount("/", routes![increase_stock])
         .mount("/", routes![decrease_stock])
         .mount("/", routes![borrow_book])
+        .mount("/", routes![return_book])
+        .mount("/", routes![sell_book])
+        .mount("/", routes![get_next_period_books])
+
 }
 
 fn totp_gen() -> Vec<String> {
